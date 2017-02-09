a57c7467e865aeffd3d04c4cba37a9fde73cc768
Martin Disch <martindisch@gmail.com>
2017-02-09 15:33:59 +0100

    Basic GATT characteristic reading

--- app/src/main/java/com/martindisch/accelerometer/DeviceFragment.java
+++ app/src/main/java/com/martindisch/accelerometer/DeviceFragment.java
@@ -1,11 +1,22 @@
 package com.martindisch.accelerometer;
 
-
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.UUID;
 
 /**
  * Fragment showing data for a connected device.
@@ -14,6 +25,11 @@ public class DeviceFragment extends Fragment {
 
     private static final String ARG_ADDRESS = "address";
     private String mAddress;
+
+    private BluetoothAdapter mBluetoothAdapter;
+    private BluetoothGatt mGatt;
+    private BluetoothGattService mTempService;
+    private BluetoothGattCharacteristic mRead;
 
     /**
      * Mandatory empty constructor.
@@ -40,6 +56,91 @@ public class DeviceFragment extends Fragment {
         if (getArguments() != null) {
             mAddress = getArguments().getString(ARG_ADDRESS);
         }
+
+        // initialize bluetooth manager & adapter
+        BluetoothManager manager = (BluetoothManager) getActivity().getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = manager.getAdapter();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        connectDevice(mAddress);
+    }
+
+    @Override
+    public void onPause() {
+        mGatt.disconnect();
+        super.onPause();
+    }
+
+    /**
+     * Creates a GATT connection to the given device.
+     *
+     * @param address String containing the address of the device
+     */
+    private void connectDevice(String address) {
+        if (!mBluetoothAdapter.isEnabled()) {
+            Toast.makeText(getActivity(), R.string.state_off, Toast.LENGTH_SHORT).show();
+            getActivity().finish();
+        }
+        BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
+        mGatt = device.connectGatt(getActivity(), false, mCallback);
+    }
+
+    private BluetoothGattCallback mCallback = new BluetoothGattCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            super.onConnectionStateChange(gatt, status, newState);
+            switch (newState) {
+                case BluetoothGatt.STATE_CONNECTED:
+                    mGatt.discoverServices();
+                    break;
+                case BluetoothGatt.STATE_DISCONNECTED:
+                    // TODO: return to ScanFragment
+                    break;
+            }
+        }
+
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            super.onServicesDiscovered(gatt, status);
+            mTempService = mGatt.getService(UUID.fromString("F000AA00-0451-4000-B000-000000000000"));
+            BluetoothGattCharacteristic enable = mTempService.getCharacteristic(UUID.fromString("F000AA02-0451-4000-B000-000000000000"));
+            enable.setValue(0x01, BluetoothGattCharacteristic.FORMAT_UINT8, 0);
+            mGatt.writeCharacteristic(enable);
+        }
+
+        @Override
+        public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            super.onCharacteristicWrite(gatt, characteristic, status);
+            mRead = mTempService.getCharacteristic(UUID.fromString("F000AA01-0451-4000-B000-000000000000"));
+            mGatt.readCharacteristic(mRead);
+        }
+
+        @Override
+        public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            super.onCharacteristicRead(gatt, characteristic, status);
+            Log.e("FFF", "Read: " + bytesToHex(characteristic.getValue()));
+            mGatt.readCharacteristic(mRead);
+        }
+    };
+
+    /**
+     * Returns a String containing the hexadecimal representation of the given byte array.
+     *
+     * @param bytes the byte array to convert
+     * @return String containing the hexadecimal representation
+     */
+    public static String bytesToHex(byte[] bytes) {
+        final char[] hexArray = "0123456789ABCDEF".toCharArray();
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
     }
 
     @Override
