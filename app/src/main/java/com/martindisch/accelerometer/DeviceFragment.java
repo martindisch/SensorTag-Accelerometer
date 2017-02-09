7b2d9a16f7fabe2e742d67624289302291554656
Martin Disch <martindisch@gmail.com>
2017-02-09 15:58:55 +0100

    Reformatting

--- app/src/main/java/com/martindisch/accelerometer/DeviceFragment.java
+++ app/src/main/java/com/martindisch/accelerometer/DeviceFragment.java
@@ -34,7 +34,8 @@ public class DeviceFragment extends Fragment {
     /**
      * Mandatory empty constructor.
      */
-    public DeviceFragment() {}
+    public DeviceFragment() {
+    }
 
     /**
      * Returns a new instance of this Fragment.
@@ -135,7 +136,7 @@ public class DeviceFragment extends Fragment {
     public static String bytesToHex(byte[] bytes) {
         final char[] hexArray = "0123456789ABCDEF".toCharArray();
         char[] hexChars = new char[bytes.length * 2];
-        for ( int j = 0; j < bytes.length; j++ ) {
+        for (int j = 0; j < bytes.length; j++) {
             int v = bytes[j] & 0xFF;
             hexChars[j * 2] = hexArray[v >>> 4];
             hexChars[j * 2 + 1] = hexArray[v & 0x0F];
