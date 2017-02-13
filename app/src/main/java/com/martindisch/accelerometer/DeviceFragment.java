e222f4efc5e35d48c58d60c22ae5bb619fc40964
Martin Disch <martindisch@gmail.com>
2017-02-13 08:10:40 +0100

    Write recording to file and share

--- app/src/main/java/com/martindisch/accelerometer/DeviceFragment.java
+++ app/src/main/java/com/martindisch/accelerometer/DeviceFragment.java
@@ -8,9 +8,12 @@ import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,6 +26,10 @@ import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -214,7 +221,28 @@ public class DeviceFragment extends Fragment implements View.OnClickListener {
                 stopRecording();
                 break;
             case R.id.bExport:
-                // TODO: create CSV data and share intent
+                File outputDir = getActivity().getCacheDir();
+                try {
+                    // create and write output file in cache directory
+                    File outputFile = File.createTempFile("recording", "csv", outputDir);
+                    OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(outputFile));
+                    writer.write(Util.recordingToCSV(mRecording));
+                    writer.close();
+
+                    // get Uri from FileProvider
+                    Uri contentUri = FileProvider.getUriForFile(getActivity(), "com.martindisch.accelerometer.fileprovider", outputFile);
+
+                    // create sharing intent
+                    Intent shareIntent = new Intent();
+                    shareIntent.setAction(Intent.ACTION_SEND);
+                    // temp permission for receiving app to read this file
+                    shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    shareIntent.setDataAndType(contentUri, getActivity().getContentResolver().getType(contentUri));
+                    shareIntent.putExtra(Intent.EXTRA_STREAM, contentUri);
+                    startActivity(Intent.createChooser(shareIntent, "Choose an app"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 break;
         }
     }
